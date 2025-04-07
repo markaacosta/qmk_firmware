9b29a4ffe5bfdc5c04cc5b2fee792a0ab233fc0d
Mark Acosta <markaacosta@gmail.com>
2025-04-07 08:55:26 -0600

    rename layer 'xtras' to 'extras'

--- keyboards/durgod/k310/keymaps/markaacosta/keymap.c
+++ keyboards/durgod/k310/keymaps/markaacosta/keymap.c
@@ -20,7 +20,7 @@
 enum layer_names {
     _main_layer,
     _media_layer,
-    _xtras_layer,
+    _extras_layer,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -32,9 +32,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // qwerty - 21
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, KC_END, KC_PGDN, KC_P7, KC_P8, KC_P9, KC_PPLS,
         // asdfgh - 17
-        KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT, KC_P4, KC_P5, KC_P6,
+        TG(_extras_layer), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT, KC_P4, KC_P5, KC_P6,
         // zxcvbn - 18
-        KC_LSFT, KC_NUBS, KC_Z, LT(_xtras_layer, KC_X), KC_C, KC_V, KC_B, KC_N, LT(_media_layer, KC_M), KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_P1, KC_P2, KC_P3, KC_PENT,
+        KC_LSFT, KC_NUBS, KC_Z, LT(_extras_layer, KC_X), KC_C, KC_V, KC_B, KC_N, LT(_media_layer, KC_M), KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_P1, KC_P2, KC_P3, KC_PENT,
         // modifiers, space, etc. - 13
         KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(1), KC_APP, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_P0, KC_PDOT),
     [_media_layer]  = LAYOUT_all(
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, KC_MEDIA_PLAY_PAUSE, _______, _______, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, _______, _______, _______, _______, _______, _______, _______,
         // _______, _______, etc. - 13
         _______, _______, _______, _______, _______, GU_TOGG, _______, _______, _______, _______, _______, _______, _______),
-    [_xtras_layer]  = LAYOUT_all(
+    [_extras_layer]  = LAYOUT_all(
         // fn keys - 16
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         // num keys - 21
