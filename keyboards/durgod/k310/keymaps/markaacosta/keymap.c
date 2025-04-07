f80d4998e4007686677a97cec907d05c851c56dc
Mark Acosta <markaacosta@gmail.com>
2025-04-07 13:40:12 -0600

    normal fn layer for function keys on numbers

--- keyboards/durgod/k310/keymaps/markaacosta/keymap.c
+++ keyboards/durgod/k310/keymaps/markaacosta/keymap.c
@@ -19,7 +19,7 @@
 // and just use numbers.
 enum layer_names {
     _main_layer,
-    _media_layer,
+    _function_layer,
     _extras_layer,
 };
 
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // fn keys - 16
         MT(MOD_LCTL, KC_ESC), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCRL, KC_PAUS,
         // num keys - 21
-        KC_GRV, NUM_F1, NUM_F2, NUM_F3, NUM_F4, NUM_F5, NUM_F6, NUM_F7, NUM_F8, NUM_F9, NUM_F10, KC_MINS, KC_EQL, KC_BSPC, KC_INS, KC_HOME, KC_PGUP, KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
+        KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_INS, KC_HOME, KC_PGUP, KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
         // qwerty - 21
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, KC_END, KC_PGDN, KC_P7, KC_P8, KC_P9, KC_PPLS,
         // asdfgh - 17
@@ -51,12 +51,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // zxcvbn - 18
         KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_P1, KC_P2, KC_P3, KC_PENT,
         // modifiers, space, etc. - 13
-        KC_LCTL, KC_LGUI, MT(MOD_LALT, KC_CAPS_LOCK), KC_SPC, KC_RALT, MO(1), KC_APP, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_P0, KC_PDOT),
-    [_media_layer]  = LAYOUT_all(
+        KC_LCTL, KC_LGUI, MT(MOD_LALT, KC_CAPS_LOCK), KC_SPC, KC_RALT, MO(_function_layer), KC_APP, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_P0, KC_PDOT),
+    [_function_layer]  = LAYOUT_all(
         // fn keys - 16
         _______, KC_MPLY, KC_MSTP, KC_MPRV, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______,
         // num keys - 21
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______, _______, _______, _______, _______, _______, _______,
         // qwerty - 21
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         // asdfgh - 17
