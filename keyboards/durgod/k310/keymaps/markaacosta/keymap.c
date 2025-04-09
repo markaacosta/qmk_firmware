1d8a4c16117a0ed0255601a30341eaf812a694eb
Mark Acosta <markaacosta@gmail.com>
2025-04-08 19:43:00 -0600

    revert escape key to normal escape functionality

--- keyboards/durgod/k310/keymaps/markaacosta/keymap.c
+++ keyboards/durgod/k310/keymaps/markaacosta/keymap.c
@@ -80,7 +80,7 @@ combo_t key_combos[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_main_layer]  = LAYOUT_all(
         // fn keys - 16
-        MT(MOD_LCTL, KC_ESC), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCRL, KC_PAUS,
+        KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCRL, KC_PAUS,
         // num keys - 21
         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_INS, KC_HOME, KC_PGUP, KC_NUM, KC_PSLS, KC_PAST, KC_PMNS,
         // qwerty - 21
