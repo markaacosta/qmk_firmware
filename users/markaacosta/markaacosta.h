780820dfd8f3dec6780466a5c48b44e3529ed286
Mark Acosta <markaacosta@gmail.com>
2025-05-13 04:54:15 -0600

    moved all logic / functionality to userspace, keychron q1v2 "keymap.c" now includes only layout setup

--- users/markaacosta/markaacosta.h
+++ users/markaacosta/markaacosta.h
@@ -21,4 +21,44 @@ enum my_layers {
     _numpad_layer,
 };
 
+enum custom_keycodes {
+    CTL_DEL = SAFE_RANGE,
+    BACKSPC_DEL,
+    NUM_F1,
+    NUM_F2,
+    NUM_F3,
+    NUM_F4,
+    NUM_F5,
+    NUM_F6,
+    NUM_F7,
+    NUM_F8,
+    NUM_F9,
+    NUM_F10,
+};
+
+// TAP DANCE
+
+enum {
+    TD_ALT_CAPS,
+    TD_SUPER_NUMPAD_LAYER,
+    TD_SHIFT_HOME,
+    TD_SHIFT_END,
+};
+
+#define NUM_TAP_DANCE_ACTIONS 4
+
+tap_dance_action_t tap_dance_actions[NUM_TAP_DANCE_ACTIONS];
+
+void td_fn_shift_home(tap_dance_state_t *state, void *user_data);
+void td_fn_shift_end(tap_dance_state_t *state, void *user_data);
+
+// KEY COMBO
+
+#define NUM_KEY_COMBOS 0
+#define NUM_KEY_COMBO_ARGS 4
+
+const uint16_t PROGMEM vol_down_combo[NUM_KEY_COMBO_ARGS];
+const uint16_t PROGMEM vol_up_combo[NUM_KEY_COMBO_ARGS];
+combo_t                key_combos[NUM_KEY_COMBOS];
+
 #endif // !USERSPACE
