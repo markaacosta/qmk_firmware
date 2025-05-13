725248318b838511ab7344b8d23762d20a978b65
Mark Acosta <markaacosta@gmail.com>
2025-05-13 02:56:55 -0600

    working first version for shared, inter-layout, inter-board USERSPACE. common configuration for layer names, functions, etc.

--- keyboards/keychron/q1v2/ansi_encoder/keymaps/keychron-encoder-custom/keymap.c
+++ keyboards/keychron/q1v2/ansi_encoder/keymaps/keychron-encoder-custom/keymap.c
@@ -16,30 +16,12 @@
 
 // for debugging / print statements / etc.
 #include "print.h"
-
-#define TAPPING_TERM 200
+// include shared USERSPACE configuration
+#include "users/markaacosta/markaacosta.h"
 
 #include QMK_KEYBOARD_H
 
 // clang-format off
-
-// Each layer gets a name for readability.
-// The underscores don't mean anything - you can
-// have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same
-// length, and you can also skip them entirely
-// and just use numbers.
-enum layers{
-    MAC_BASE,
-    MAC_FN,
-    // original name: WIN_BASE
-    _main_layer,
-    // original name: WIN_FN
-    _function_layer,
-    _extras_layer,
-    _mouse_layer,
-    _numpad_layer,
-};
 
 enum custom_keycodes {
     CTL_DEL = SAFE_RANGE,
---
+++ users/markaacosta/readme.md
@@ -0,0 +1,5 @@
+# qmk userspace
+
+> shared userspace / functionality for qmk-supported boards
+
+as seen [here](https://docs.qmk.fm/feature_userspace#userspace-sharing-code-between-keymaps), qmk supports a shared userspace for different boards / maps
